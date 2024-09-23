9d46f89daf5dc1900e88c8e7d28d50975566cc73
ciara <g00402357@atu.ie>
2024-09-23 17:25:39 +0100

    added get and set method

---
+++ src/main/java/ie/atu/ie.atu.week2_refresher/ProductController.java
@@ -0,0 +1,26 @@
+package ie.atu.ie.atu.week2_refresher;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/test")
+public class ProductController {
+
+    private List<Product> listproduct = new ArrayList<Product>();
+
+    @GetMapping("/products")
+    public List<Product> getProductList()
+    {
+        return listproduct;
+    }
+
+    @PostMapping("/products")
+    public Product returnProducts(@RequestBody Product product)
+    {
+        listproduct.add(product);
+        return product;
+    }
+}
